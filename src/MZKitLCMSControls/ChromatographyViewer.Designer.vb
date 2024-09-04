69f04ac7f479c9653b2dabaae09700189eb9da03
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-04 10:33:23 +0800

    ChromatographyViewer

---
+++ src/MZKitLCMSControls/ChromatographyViewer.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class ChromatographyViewer
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl1 overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        PictureBox1 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox1.Dock = DockStyle.Fill
+        PictureBox1.Location = New Point(0, 0)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(800, 450)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' ChromatographyViewer
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        Controls.Add(PictureBox1)
+        Name = "ChromatographyViewer"
+        Size = New Size(800, 450)
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+
+End Class
---
+++ src/MZKitLCMSControls/ChromatographyViewer.vb
@@ -0,0 +1,61 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Chromatogram
+Imports BioNovoGene.Analytical.MassSpectrometry.Visualization
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.MIME.Html.CSS
+
+Public Class ChromatographyViewer
+
+    ''' <summary>
+    ''' all Chromatography data points
+    ''' </summary>
+    Dim chromatography As ChromatogramTick()
+    Dim plotPadding As Padding = $"padding: 100px 100px 200px 250px;"
+
+    Public Property XLabel As String = "Rentention Time(s)"
+    Public Property YLabel As String = "Intensity"
+
+    ''' <summary>
+    ''' title name of the Chromatography data
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Title As String
+
+    ''' <summary>
+    ''' Other Chromatography data overlaps with current Chromatography data 
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Overlaps As NamedCollection(Of ChromatogramTick)()
+
+    Private Sub Rendering()
+        Dim chromatography As TICplot
+        Dim theme As New Theme With {.padding = plotPadding.ToString}
+        Dim scale As Double = 2.5
+        Dim size_str As String = $"{PictureBox1.Width * scale},{PictureBox1.Height * scale}"
+
+        If Overlaps.IsNullOrEmpty Then
+            chromatography = New TICplot(New NamedCollection(Of ChromatogramTick)(Title, Me.chromatography), Nothing, 0, isXIC:=True, fillAlpha:=255, fillCurve:=False, labelLayoutTicks:=-1, bspline:=2, theme:=theme)
+        Else
+            Dim overlaps = _Overlaps.Join(New NamedCollection(Of ChromatogramTick)(Title, Me.chromatography))
+            chromatography = New TICplot(overlaps, Nothing, 0, isXIC:=True, fillAlpha:=255, fillCurve:=False, labelLayoutTicks:=-1, bspline:=2, theme:=theme)
+        End If
+
+        chromatography.xlabel = XLabel
+        chromatography.ylabel = YLabel
+
+        PictureBox1.BackgroundImage = chromatography.Plot(size_str, ppi:=120).AsGDIImage
+    End Sub
+
+    Public Sub SetChromatography(data As IEnumerable(Of ChromatogramTick))
+        chromatography = data.ToArray
+    End Sub
+
+    Public Sub SetPlotPadding(padding As Padding)
+        plotPadding = padding
+    End Sub
+
+    Private Sub PictureBox1_SizeChanged(sender As Object, e As EventArgs) Handles PictureBox1.SizeChanged
+        Call Rendering()
+    End Sub
+End Class
