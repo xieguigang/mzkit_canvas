8607fc8a5ddeb6c7bde810e94405f7647cd60932
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-28 10:22:59 +0800

    update source file banner

--- src/MZKitLCMSControls/ChromatographyViewer.vb
+++ src/MZKitLCMSControls/ChromatographyViewer.vb
@@ -1,4 +1,64 @@
-﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Chromatogram
+﻿#Region "Microsoft.VisualBasic::ec48dc9284be1602c2c4e73904607d1a, mzkit\ux\Canvas\src\MZKitLCMSControls\ChromatographyViewer.vb"
+
+    ' Author:
+    ' 
+    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+    ' 
+    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+    ' 
+    ' 
+    ' MIT License
+    ' 
+    ' 
+    ' Permission is hereby granted, free of charge, to any person obtaining a copy
+    ' of this software and associated documentation files (the "Software"), to deal
+    ' in the Software without restriction, including without limitation the rights
+    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    ' copies of the Software, and to permit persons to whom the Software is
+    ' furnished to do so, subject to the following conditions:
+    ' 
+    ' The above copyright notice and this permission notice shall be included in all
+    ' copies or substantial portions of the Software.
+    ' 
+    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    ' SOFTWARE.
+
+
+
+    ' /********************************************************************************/
+
+    ' Summaries:
+
+
+    ' Code Statistics:
+
+    '   Total Lines: 139
+    '    Code Lines: 100 (71.94%)
+    ' Comment Lines: 12 (8.63%)
+    '    - Xml Docs: 91.67%
+    ' 
+    '   Blank Lines: 27 (19.42%)
+    '     File Size: 5.32 KB
+
+
+    ' Class ChromatographyViewer
+    ' 
+    '     Properties: ChromatographyData, ChromatographyPlot, Overlaps, Title, XLabel
+    '                 YLabel
+    ' 
+    '     Sub: PictureBox1_MouseMove, PictureBox1_SizeChanged, Rendering, SetChromatography, SetPlotPadding
+    '          TranslateViewLocation
+    ' 
+    ' /********************************************************************************/
+
+#End Region
+
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Chromatogram
 Imports BioNovoGene.Analytical.MassSpectrometry.Visualization
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Axis
@@ -137,3 +197,4 @@ Public Class ChromatographyViewer
         End Using
     End Sub
 End Class
+
