9d0ec27b23388f7d2f46a3065e62a2194acd4d3a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-11 12:52:53 +0800

    view alignment?

---
+++ src/MZKitLCMSControls/SpectrumAlignmentViewer.vb
@@ -0,0 +1,21 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra.Xml
+
+Public Class SpectrumAlignmentViewer
+
+    Public Property alignment As AlignmentOutput
+        Get
+            Return m_alignment
+        End Get
+        Set(value As AlignmentOutput)
+            m_alignment = value
+            Rendering()
+        End Set
+    End Property
+
+    Dim m_alignment As AlignmentOutput
+
+    Private Sub Rendering()
+
+    End Sub
+
+End Class
