b171c3a8b7f485693171cbbeb3173040029e1469
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-11 14:44:05 +0800

    do plot test

--- src/test/spectrumViewerForm1.vb
+++ src/test/spectrumViewerForm1.vb
@@ -1,3 +1,12 @@
-﻿Public Class spectrumViewerForm1
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
 
+Public Class spectrumViewerForm1
+    Private Sub spectrumViewerForm1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim demo As New LibraryMatrix("test spectrum", {
+            New ms2(53, 100),
+            New ms2(129, 33)
+        })
+
+        Call MassSpectrometryViewer1.SetSpectrum(demo)
+    End Sub
 End Class
